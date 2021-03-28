41ea08f88155661b6516cbda9896dd90b875ae72
Løvgren, Dag Erik <dagerikhl@gmail.com>
2021-03-28 13:47:48 +0200

    build(app): add possibility to launch with command line args

--- scripts/launch.js
+++ scripts/launch.js
@@ -3,15 +3,31 @@ const path = require("path");
 
 const { getAddonName, getDotaPath } = require("./utils");
 
+const commandLineArgs = process.argv.slice(2);
+
+const addonName = getAddonName();
+
 (async () => {
   const dotaPath = await getDotaPath();
   const win64 = path.join(dotaPath, "game", "bin", "win64");
 
-  // You can add any arguments there
-  // For example `+dota_launch_custom_game ${getAddonName()} dota` would automatically load "dota" map
-  const args = ["-novid", "-tools", "-addon", getAddonName()];
+  /*
+   You can add any arguments here.
+   E.g. `-dota_launch_custom_game ${getAddonName()} dota` would automatically load the "dota" map.
+   You can also add command line args when running the script.
+   E.g. `yarn launch -language norwegian` would launch the game with norwegian localization.
+   */
+  const args = [
+    "-novid",
+    "-tools",
+    `-addon ${addonName}`,
+    // `-dota_launch_custom_game ${addonName} ${addonName}`,
+    ...commandLineArgs,
+  ];
+  console.log(`Launching game with args \`${args.join(" ")}\``);
   spawn(path.join(win64, "dota2.exe"), args, { detached: true, cwd: win64 });
 })().catch((error) => {
   console.error(error);
+
   process.exit(1);
 });
